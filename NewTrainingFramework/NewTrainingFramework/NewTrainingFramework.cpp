545e26c06892d85a7151891950c2cfbbc21fb86a
HoanxHoan <hoan8e1@gmail.com>
2025-07-07 10:42:27 +0700

    +Model_Woman1

--- NewTrainingFramework/NewTrainingFramework/NewTrainingFramework.cpp
+++ NewTrainingFramework/NewTrainingFramework/NewTrainingFramework.cpp
@@ -1,4 +1,4 @@
-// NewTrainingFramework.cpp : Defines the entry point for the console application.
+﻿// NewTrainingFramework.cpp : Defines the entry point for the console application.
 //
 
 #include "stdafx.h"
@@ -7,65 +7,125 @@
 #include "Shaders.h"
 #include "Globals.h"
 #include <conio.h>
-
-
+#include <cstdio>   // fopen_s, fscanf, fclose
+#include <iostream>
 GLuint vboId;
 GLuint iboId;
 GLuint textureID;
 Shaders myShaders;
 int iWidth, bpp, iHeight;
 
+void LoadNFG(const char* filename, Vertex* verticesData, unsigned int* indices)
+{
+	FILE* f;
+
+	if (fopen_s(&f, filename, "rb") != 0)
+	{
+		std::cout << "Cannot open file.\n";
+		return;
+	}
+
+	char temp[256];
+	int nrVertices = 0;
+	fscanf_s(f, "%s %d", temp, sizeof(temp), &nrVertices); // NrVertices: <num>
+
+	
+	for (int i = 0; i < nrVertices; i++)
+	{
+		int index;
+		Vertex v;
+
+		fscanf_s(f, "%d.", &index); 
+
+		
+		fscanf_s(f, " pos:[%f, %f, %f];", &v.pos.x, &v.pos.y, &v.pos.z);
+		
+		fscanf_s(f, " norm:[%f, %f, %f];", &v.normal.x, &v.normal.y, &v.normal.z);
+		
+		fscanf_s(f, " binorm:[%f, %f, %f];", &v.binormal.x, &v.binormal.y, &v.binormal.z);
+		
+		fscanf_s(f, " tgt:[%f, %f, %f];", &v.tangent.x, &v.tangent.y, &v.tangent.z);
+		
+		fscanf_s(f, " uv:[%f, %f];", &v.uv.x, &v.uv.y);
+
+		verticesData[i] = v;
+	}
+
+	int nrIndices = 0;
+	fscanf_s(f, "%s %d", temp, sizeof(temp), &nrIndices); // NrIndices: <num>
+
+	
+	for (int i = 0; i < nrIndices / 3; i++)
+	{
+		int idxLine;
+		unsigned int a, b, c;
+
+		fscanf_s(f, "%d.", &idxLine);
+		fscanf_s(f, " %d, %d, %d", &a, &b, &c);
+
+		indices[i * 3 + 0] = a;
+		indices[i * 3 + 1] = b;
+		indices[i * 3 + 2] = c;
+	}
+
+	fclose(f);
+
+	std::cout << "Loaded " << nrVertices << " vertices and " << nrIndices << " indices.\n";
+}
 
 int Init ( ESContext *esContext )
 {
 	glClearColor ( 0.0f, 0.0f, 0.0f, 0.0f );
 
 	//triangle data (heap)
-	Vertex verticesData[3];
+	Vertex* verticesData= new Vertex[512];
+	unsigned int* indices= new unsigned int[2154];
 
-	verticesData[0].pos.x =  0.0f;  verticesData[0].pos.y =  0.5f;  verticesData[0].pos.z =  0.0f;
+	LoadNFG("../Resources/Models/Woman1.nfg", verticesData, indices);
+
+
+
+	/*verticesData[0].pos.x =  0.0f;  verticesData[0].pos.y =  0.5f;  verticesData[0].pos.z =  0.0f;
 	verticesData[1].pos.x = -0.5f;  verticesData[1].pos.y = -0.5f;  verticesData[1].pos.z =  0.0f;
-	verticesData[2].pos.x =  0.5f;  verticesData[2].pos.y = -0.5f;  verticesData[2].pos.z =  0.0f;
+	verticesData[2].pos.x =  0.5f;  verticesData[2].pos.y = -0.5f;  verticesData[2].pos.z =  0.0f;*/
 
 	//verticesData[0].color.x = 1.0f;  verticesData[0].color.y = 0.0f;  verticesData[0].color.z = 0.0f;
 	//verticesData[1].color.x = 0.0f;  verticesData[1].color.y = 1.0f;  verticesData[1].color.z = 0.0f;
 	//verticesData[2].color.x = 0.0f;  verticesData[2].color.y = 0.0f;  verticesData[2].color.z = 0.1f;
 
 
-	verticesData[0].uv.x = 1.0f;  verticesData[0].uv.y = 0.0f; 
+	/*verticesData[0].uv.x = 1.0f;  verticesData[0].uv.y = 0.0f; 
 	verticesData[1].uv.x = 0.0f;  verticesData[1].uv.y = 1.0f; 
-	verticesData[2].uv.x = 1.0f;  verticesData[2].uv.y = 1.0f; 
+	verticesData[2].uv.x = 1.0f;  verticesData[2].uv.y = 1.0f; */
 	
 
 
-	unsigned int indices[] = {
-		0, 1, 2
-	};
+	//unsigned int indices[] = { 0,1,2 };
 
 	//buffer object
 
 	glGenBuffers(1, &vboId);
 	glBindBuffer(GL_ARRAY_BUFFER, vboId);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(verticesData), verticesData, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, 512 * sizeof(Vertex), verticesData, GL_STATIC_DRAW);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
 	glGenBuffers(1, &iboId);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, iboId);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, 2154 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
 	glGenTextures(1, &textureID);
 	glBindTexture(GL_TEXTURE_2D, textureID);
 	char* imageData = LoadTGA("../Resources/Textures/Woman1.tga", &iWidth, &iHeight, &bpp);
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, iWidth, iHeight, 0,GL_RGB, GL_UNSIGNED_BYTE, imageData);
-	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,GL_NEAREST_MIPMAP_NEAREST);
-
 	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	glGenerateMipmap(GL_TEXTURE_2D);
-
+	delete[] verticesData;
+	delete[] indices;
 	//creation of shaders and program 
 	return myShaders.Init("../Resources/Shaders/TriangleShaderVS.vs", "../Resources/Shaders/TriangleShaderFS.fs");
 
@@ -89,14 +149,19 @@ void Draw ( ESContext *esContext )
 	{
 		glEnableVertexAttribArray(0);
 		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), 0);
+		glEnableVertexAttribArray(1);
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (char*)0 + sizeof(Vector3));
+		glEnableVertexAttribArray(2);
+		glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (char*)12 + sizeof(Vector3));
+		glEnableVertexAttribArray(3);
+		glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (char*)24 + sizeof(Vector3));
+		glEnableVertexAttribArray(4);
+		glVertexAttribPointer(4, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (char*)48);
 	}
-	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (char*)0 + sizeof(Vector2));
+
 
 	//glDrawArrays(GL_TRIANGLES, 0, 3);
-	//glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
-
-	glDrawArrays(GL_TRIANGLES, 0, 3);
+	glDrawElements(GL_TRIANGLES, 2154, GL_UNSIGNED_INT, 0);
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
